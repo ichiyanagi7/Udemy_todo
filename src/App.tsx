4a226ee4fa506133b55242262fe8ff770cc2d90e
ichiyanagi7 <ichiyanagi1024@gmail.com>
2021-12-15 15:47:05 +0900

    task入力追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
+import { FormControl, TextField } from '@material-ui/core';
 import React, { useState, useEffect } from 'react';
 import './App.css';
 import { db } from "./firebase";
+import AddToPhotosIcon from "@material-ui/icons/AddToPhotos";
 
 const App: React.FC = () => {
   const [tasks, setTasks] = useState([{id: "", title: ""}]);
+  const [input, setInput] = useState("");
 
   useEffect(() => {
     const unSub = db.collection("tasks").onSnapshot((snapshot) => {
@@ -17,6 +20,18 @@ const App: React.FC = () => {
 
   return (
     <div className="App">
+      <h1>Todo App by React/Firebase</h1>
+      <FormControl>
+        <TextField
+          InputLabelProps={{
+            shrink: true,
+          }}
+          label="New task ?"
+          value={input}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) => setInput(e.target.value)}
+        />
+      </FormControl>
+
       {tasks.map((task) => (
         <h3 key={task.id}>{task.title}</h3>
       ))}
